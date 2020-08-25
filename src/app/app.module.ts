78e32dfa44174099cad9457aaf07cd9702074597
Vijayender Thakur <thakurvijayender@gmail.com>
2020-08-26 02:08:34 +0530

    Working on components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IntroComponent,
+    AboutComponent,
+    ServicesComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
